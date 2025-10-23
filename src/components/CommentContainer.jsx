3034d4cb1159b2dd7ec608d656c158c6c410e423
abedin-ansari <128270898+abedin-ansari@users.noreply.github.com>
2025-10-23 23:56:23 +0530

    CommentContainer Start Implementing Comment Feature

---
+++ src/components/CommentContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CommentContainer = () => {
+  return (
+    <div className="m-3 p-2">
+      <h1>Comments</h1>
+    </div>
+  );
+};
+
+export default CommentContainer;
